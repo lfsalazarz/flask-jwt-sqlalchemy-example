caaae4430a09599869f10eb479fbbefc12aea8e7
Desarrollo 2 <desarrollo2@MacBook-Pro-de-Desarrollo.local>
2020-05-13 16:31:27 -0500

    code format with black

--- app.py
+++ app.py
@@ -80,32 +80,26 @@ def add_claims_to_jwt(identity):
 # https://flask-jwt-extended.readthedocs.io/en/latest/changing_default_behavior.html#changing-callback-functions
 @jwt.invalid_token_loader
 def invalid_token_callback(error):
-    return jsonify({
-        "message": "Signature verification failed.",
-        "error": "invalid_token"
-    }), UNAUTHORIZED
+    return jsonify({"message": "Signature verification failed.", "error": "invalid_token"}), UNAUTHORIZED
 
 
 @jwt.unauthorized_loader
 def missing_token_callback(error):
-    return jsonify({
-        "description": "Request does not contain an access token.",
-        "error": "authorization_required"
-    }), UNAUTHORIZED
+    return (
+        jsonify({"description": "Request does not contain an access token.", "error": "authorization_required"}),
+        UNAUTHORIZED,
+    )
+
 
 @jwt.expired_token_loader
 def expired_token_callback():
-    return jsonify({
-        "message": "The token has expired.",
-        "error": "token_expired"
-    }), UNAUTHORIZED
+    return jsonify({"message": "The token has expired.", "error": "token_expired"}), UNAUTHORIZED
+
 
 @jwt.needs_fresh_token_loader
 def token_not_fresh_callback():
-    return jsonify({
-        "description": "The token is not fresh.",
-        "error": "fresh_token_required"
-    }), UNAUTHORIZED
+    return jsonify({"description": "The token is not fresh.", "error": "fresh_token_required"}), UNAUTHORIZED
+
 
 # @jwt.revoked_token_loader
 @jwt.token_in_blacklist_loader
--- middlewares/middleware.py
+++ middlewares/middleware.py
@@ -10,10 +10,6 @@ class MaxContentLength:
     def __call__(self, environ, start_response):
         request = Request(environ)
         if request.content_length is not None and request.content_length > self.max_length:
-            res = Response(
-                dumps({"message": "Payload Too Large"}), 
-                content_type='application/json', 
-                status=413
-            )
+            res = Response(dumps({"message": "Payload Too Large"}), content_type="application/json", status=413)
             return res(environ, start_response)
         return self.app(environ, start_response)
